bf374252be21ae2aa5877a1ab46dea17c0ab188c
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-09 14:19:00 +0300

    Add files via upload

---
+++ Metotlar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 2;
+
+            Console.WriteLine(a+b);
+
+            int sonuc = topla(a,b);
+
+            Console.WriteLine(sonuc);
+
+
+            Metotlar ornek = new Metotlar();
+            ornek.ekranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc1 = ornek.ArttirVeTopla(a,b);
+            ornek.ekranaYazdir(Convert.ToString(sonuc1));
+
+        }
+
+       static int topla(int sayi1, int sayi2) 
+        {
+            return(sayi1 + sayi2);
+        }
+
+       
+    }
+     public class Metotlar
+        { 
+            public void ekranaYazdir(string veri)
+            {
+                Console.WriteLine(veri);
+            }
+
+
+            public int ArttirVeTopla(int deger1, int deger2)
+            {
+                deger1+=1;
+                deger2+=1;
+                return deger1 + deger2;
+            }
+
+        }
+}
